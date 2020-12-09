5398a96a5bd9a2e29a9a963b618afa0912e1bfc8
Harits Fahreza <harits.fahreza@gmail.com>
2020-12-10 00:40:07 +0700

    Remove unused tab at EOL

--- scripts/codecov-go-test-result.sh
+++ scripts/codecov-go-test-result.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/env bash	
+#!/usr/bin/env bash
 
-set -e	
-echo "" > coverage.txt	
+set -e
+echo "" > coverage.txt
 
-for d in $(go list ./... | grep -v vendor); do	
-    go test -race -coverprofile=profile.out -covermode=atomic $d	
-    if [ -f profile.out ]; then	
-        cat profile.out >> coverage.txt	
-        rm profile.out	
-    fi	
-done 
+for d in $(go list ./... | grep -v vendor); do
+    go test -race -coverprofile=profile.out -covermode=atomic $d
+    if [ -f profile.out ]; then
+        cat profile.out >> coverage.txt
+        rm profile.out
+    fi
+done
